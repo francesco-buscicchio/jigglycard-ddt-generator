7cd5c07a402ea05dddb36a5dfee4d12c099fa898
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-03-10 22:05:21 +0100

    Fixato CardTrader Generator

--- services/cardTraderService.js
+++ services/cardTraderService.js
@@ -1,28 +1,36 @@
-const axios = require('axios');
-const { CARDTRADER_TOKEN } = require('../config/config');
-const { formatDate } = require('../utils/dateUtils');
+const axios = require("axios");
+const { CARDTRADER_TOKEN } = require("../config/config");
+const { formatDate } = require("../utils/dateUtils");
 
 const headers = {
-    Authorization: `Bearer ${CARDTRADER_TOKEN}`,
+  Authorization: `Bearer ${CARDTRADER_TOKEN}`,
 };
 
 exports.fetchOrders = async () => {
-    let orders = [], page = 1, limit = 100;
-    const fromDate = formatDate(new Date(Date.now() - 2 * 24 * 60 * 60 * 1000));
+  let orders = [],
+    page = 1,
+    limit = 100;
+  const fromDate = formatDate(new Date(Date.now() - 15 * 24 * 60 * 60 * 1000));
 
-    while (true) {
-        const { data } = await axios.get('https://api.cardtrader.com/api/v2/orders', {
-            headers,
-            params: { sort: 'date.desc', from: fromDate, page, limit },
-        });
-        if (!data.length) break;
-        orders = orders.concat(data);
-        page++;
-    }
-    return orders.filter(o => o.state !== 'hub_pending');
+  while (true) {
+    const { data } = await axios.get(
+      "https://api.cardtrader.com/api/v2/orders",
+      {
+        headers,
+        params: { sort: "date.desc", from: fromDate, page, limit },
+      }
+    );
+    if (!data.length) break;
+    orders = orders.concat(data);
+    page++;
+  }
+  return orders.filter((o) => o.state !== "hub_pending");
 };
 
 exports.fetchOrderDetails = async (orderId) => {
-    const { data } = await axios.get(`https://api.cardtrader.com/api/v2/orders/${orderId}`, { headers });
-    return data;
+  const { data } = await axios.get(
+    `https://api.cardtrader.com/api/v2/orders/${orderId}`,
+    { headers }
+  );
+  return data;
 };
--- services/orderService.js
+++ services/orderService.js
@@ -1,24 +1,75 @@
-const { fetchOrders, fetchOrderDetails } = require('./cardTraderService');
-const excelService = require('./excelService');
-const fileService = require('./fileService');
-const { mapShippingAddress, mapShippingMethod, mapShippingItems } = require('../models');
+const { fetchOrders, fetchOrderDetails } = require("./cardTraderService");
+const excelService = require("./excelService");
+const fileService = require("./fileService");
+const { Address, ShippingMethod, ShippingItem } = require("../models");
 
 exports.processOrders = async () => {
-    const processedOrderIds = await fileService.getProcessedOrderIds();
-    const orders = await fetchOrders();
-    let ddtNumber = await fileService.getDDTNumber();
+  const processedOrderIds = await fileService.getProcessedOrderIds();
+  const orders = await fetchOrders();
+  let ddtNumber = await fileService.getDDTNumber();
 
-    for (const order of orders) {
-        if (processedOrderIds.includes(order.id.toString())) continue;
+  for (const order of orders) {
+    if (processedOrderIds.includes(order.id.toString())) continue;
 
-        const docAddress = mapShippingAddress(order);
-        const orderDetail = await fetchOrderDetails(order.id);
-        const shippingMethod = mapShippingMethod(order);
-        const shippingItems = mapShippingItems(orderDetail);
+    const docAddress = mapShippingAddress(order);
+    const orderDetail = await fetchOrderDetails(order.id);
+    const shippingMethod = mapShippingMethod(order);
+    const shippingItems = mapShippingItems(orderDetail);
 
-        await excelService.generateExcel(ddtNumber, docAddress, shippingItems, shippingMethod);
-        await fileService.logProcessedOrderId(order.id);
-        ddtNumber++;
-    }
-    await fileService.updateDDTNumber(ddtNumber);
+    await excelService.generateExcel(
+      ddtNumber,
+      docAddress,
+      shippingItems,
+      shippingMethod
+    );
+    await fileService.logProcessedOrderId(order.id);
+    ddtNumber++;
+  }
+  await fileService.updateDDTNumber(ddtNumber);
 };
+
+function mapShippingAddress(apiResponse) {
+  const shippingAddress = new Address({
+    name: apiResponse.order_billing_address.name,
+    street: apiResponse.order_billing_address.street,
+    zip: apiResponse.order_billing_address.zip,
+    city: apiResponse.order_billing_address.city,
+    state_or_province: apiResponse.order_billing_address.state_or_province,
+    country: apiResponse.order_billing_address.country,
+    orderCode: apiResponse.code,
+  });
+  return shippingAddress;
+}
+
+function mapShippingMethod(apiResponse) {
+  if (
+    apiResponse.order_shipping_method &&
+    apiResponse.order_shipping_method.id != 1207508
+  ) {
+    const shippingMethod = new ShippingMethod({
+      id: apiResponse.order_shipping_method.id,
+      name: apiResponse.order_shipping_method.name,
+      price: apiResponse.order_shipping_method.price
+        ? apiResponse.order_shipping_method.price.cents
+        : 0,
+    });
+
+    return shippingMethod;
+  }
+  return undefined;
+}
+
+function mapShippingItems(apiResponse) {
+  const allShippingItems = [];
+  //to do rimuovere righe non spedite
+  for (let item of apiResponse.order_items) {
+    const shippingItem = new ShippingItem({
+      name: item.name,
+      collectionNumber: item.properties.collector_number,
+      price: item.seller_price.cents,
+      quantity: item.quantity,
+    });
+    allShippingItems.push(shippingItem);
+  }
+  return allShippingItems;
+}
