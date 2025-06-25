c6307b2b21c121085021cc3763908883244b28ad
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-06-25 21:44:48 +0200

    Fix Timeout Sniffer

--- controllers/snifferController.js
+++ controllers/snifferController.js
@@ -2,12 +2,20 @@ const snifferService = require("../services/snifferService");
 
 exports.sniffCardtraderProducts = async (req, res) => {
   try {
-    await snifferService.sniffCardtraderProducts();
-    res.status(200).send("Sniffed successfully for Cardtrader products.");
+    snifferService
+      .sniffCardtraderProducts()
+      .then(() => {
+        console.log("SniffCardtraderProducts completed successfully");
+      })
+      .catch((error) => {
+        console.error("Sniffer error:", error);
+      });
+
+    res.status(202).send("Sniffer started for Cardtrader products.");
   } catch (error) {
     console.error(error);
     res
       .status(500)
-      .send("An error occurred while processing Cardtrader sniffer.");
+      .send("An error occurred while initiating Cardtrader sniffer.");
   }
 };
