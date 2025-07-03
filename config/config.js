dd9fabb360c5654a76acaf98edb5ee14b77085a9
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2025-07-03 13:31:18 +0200

    Porta Server

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 module.exports = {
-  PORT: process.env.PORT || 3005,
+  PORT: process.env.PORT || 3000,
   CARDTRADER_TOKEN: process.env.CARDTRADER_TOKEN,
   EXPORT_FOLDER: process.env.EXPORT_FOLDER,
   PROCESSED_ORDERS_FILE: "processed_orders.txt",
